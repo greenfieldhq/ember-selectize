d0e1de01387f260e19c75b9cf13e822a96164aab
Eric Kelly <heroiceric@gmail.com>
2015-06-02 10:31:40 -0400

    Don't use ES2015 in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 module.exports = {
   name: 'ember-selectize',
 
-  included(app) {
+  included: function(app) {
     this._super.included(app);
 
     app.import(`${app.bowerDirectory}/selectize/dist/js/standalone/selectize.js`);
